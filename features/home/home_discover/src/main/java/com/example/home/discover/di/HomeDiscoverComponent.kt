47d76503df637c4d895519d9aae777ccb70256db
bayazidsustami <bayazidsustamy@gmail.com>
2024-03-28 15:19:14 +0700

    feat : complete binding repository

--- features/home/home_discover/src/main/java/com/example/home/discover/di/HomeDiscoverComponent.kt
+++ features/home/home_discover/src/main/java/com/example/home/discover/di/HomeDiscoverComponent.kt
@@ -1,14 +1,17 @@
 package com.example.home.discover.di
 
+import com.example.common.coroutine.DispatcherModule
 import com.example.core.network.di.NetworkComponent
 import com.example.home.discover.data.datasource.HomeDiscoverDataSourceModule
+import com.example.home.discover.data.repository.HomeDiscoverRepositoryModule
 import dagger.Component
 
 @HomeDiscover
 @Component(
     modules = [
         HomeDiscoverDataSourceModule::class,
-    
+        DispatcherModule::class,
+        HomeDiscoverRepositoryModule::class,
     ],
     dependencies = [
         NetworkComponent::class
