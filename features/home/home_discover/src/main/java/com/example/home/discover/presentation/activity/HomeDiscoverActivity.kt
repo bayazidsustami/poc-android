e5a85eae306a7199e62ebeef41a401d80144dae3
bayazidsustami <bayazidsustamy@gmail.com>
2024-04-02 10:01:37 +0700

    feat : complete observe data

--- features/home/home_discover/src/main/java/com/example/home/discover/presentation/activity/HomeDiscoverActivity.kt
+++ features/home/home_discover/src/main/java/com/example/home/discover/presentation/activity/HomeDiscoverActivity.kt
@@ -4,17 +4,23 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.core.applications.coreComponent
 import com.example.home.discover.R
 import com.example.home.discover.di.DaggerHomeDiscoverComponent
 import com.example.home.discover.presentation.viewmodel.HomeDiscoverViewModel
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class HomeDiscoverActivity : AppCompatActivity() {
@@ -45,11 +51,23 @@ class HomeDiscoverActivity : AppCompatActivity() {
         findViewById<AppCompatTextView>(R.id.tv_click).setOnClickListener {
             startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("example://movies/movie-favorite")))
         }
+
+        populateData()
     }
 
     private fun inject() {
         DaggerHomeDiscoverComponent.factory()
             .create(coreComponent())
             .inject(this)
+    }
+
+    private fun populateData() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                viewModel.homeContent.collectLatest {
+                    Log.d("DATA", it.toString())
+                }
+            }
+        }
     }
 }
--- features/home/home_discover/src/main/java/com/example/home/discover/presentation/viewmodel/HomeDiscoverViewModel.kt
+++ features/home/home_discover/src/main/java/com/example/home/discover/presentation/viewmodel/HomeDiscoverViewModel.kt
@@ -3,7 +3,11 @@ package com.example.home.discover.presentation.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.home.discover.domain.GetHomeContentUseCase
-import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -11,9 +15,18 @@ class HomeDiscoverViewModel @Inject constructor(
     private val useCase: GetHomeContentUseCase
 ) : ViewModel() {
 
+    private val _homeContent = MutableStateFlow<HomeDiscoverUIEvent>(HomeDiscoverUIEvent.Loading)
+    val homeContent:StateFlow<HomeDiscoverUIEvent> get() = _homeContent
+
     fun loadContent() {
         viewModelScope.launch {
-            useCase.invoke().collect()
+            useCase.invoke().onStart {
+                _homeContent.value = HomeDiscoverUIEvent.Loading
+            }.catch {
+                _homeContent.value = HomeDiscoverUIEvent.Failed(it.message.orEmpty())
+            }.collectLatest {
+                _homeContent.value = HomeDiscoverUIEvent.Success(it)
+            }
         }
     }
 }
